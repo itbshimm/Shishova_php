d67ced41c094680939f6dce99ad33be4444c42f3
itshimm <itbshimm@gmail.com>
2021-12-14 23:50:36 +0500

    add form

---
+++ kursovaya/modules/request_consult.php
@@ -0,0 +1,12 @@
+<div class="request_consult">
+    <h1>ЗАКАЗАТЬ КОНСУЛЬТАЦИЮ ИНЖЕНЕРА</h1>
+    <br>
+    <form action="">
+        <input type="text">
+        <input type="text">
+        <input type="text">
+        <button>
+            ЗАКАЗАТЬ
+        </button>
+    </form>
+</div>
